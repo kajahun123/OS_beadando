7a371420e4bcfdf77611d5adc5f9266c6991a435
kajahun123 <65674181+kajahun123@users.noreply.github.com>
2020-06-01 22:36:02 +0200

    Update beadando.sh

--- beadando.sh
+++ beadando.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#! /bin/bash
 
 files_dir=$1
 
-files="$(find ${files_dir} -type f)"
-	for file1 in $files; do
-		for file2 in $files; do
-			if [[ "$file1" != "$file2" && -e "$file1" && -e "$file2" ]]; then
-				if  diff "file1" "file2" > /dev/null; then
-					echo "$file1 es $file2 duplikalva van"
-					read -p "Kiszeretned oket torolni?: " text
-						if ["$text" == "Igen"]; then
-							rm -v "$file2"
-						fi
-				fi
-			fi
-		done
-	done
-	
+files="$( find ${files_dir} -type f )"
+for file1 in $files; do
+    for file2 in $files; do
+       if [[ "$file1" != "$file2" && -e "$file1" && -e "$file2" ]]; then
+          if diff "$file1" "$file2" > /dev/null; then
+              echo "$file1 es $file2 duplikalva vannak"
+                 read -p "Kiszeretned oket torolni?: "
+                        if [ ${REPLY} == "Igen" ]; then
+                                rm -v "$file2"
+
+                        elif [ ${REPLY} == "Nem" ]; then
+                                exit
+                        fi
+         fi
+       fi
+    done
+done
